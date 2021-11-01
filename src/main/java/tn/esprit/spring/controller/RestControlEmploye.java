4dff47dd2df061d2c0288a3e7b50fd3026849b2a
moetaz.brayek <moetaz.brayek@esprit.tn>
2021-11-01 13:33:39 +0800

    Enable Cors :fire:

--- src/main/java/tn/esprit/spring/controller/RestControlEmploye.java
+++ src/main/java/tn/esprit/spring/controller/RestControlEmploye.java
@@ -4,6 +4,7 @@ import java.util.Date;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -19,7 +20,7 @@ import tn.esprit.spring.entities.Mission;
 import tn.esprit.spring.entities.Timesheet;
 import tn.esprit.spring.services.IEmployeService;
 import tn.esprit.spring.services.ITimesheetService;
-
+@CrossOrigin(origins = "http://localhost:4200")
 @RestController
 public class RestControlEmploye {
 
