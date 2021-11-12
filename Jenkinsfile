3c4e3504f9fcd8663e667e647c415caacf9505d8
skanderbac <skanderbac@users.noreply.github.com>
2021-11-12 16:43:12 +0100

    Jenkins

---
+++ Jenkinsfile
@@ -0,0 +1,87 @@
+pipeline {
+
+    environment { 
+        registry = "skanderbac/timesheet" 
+        registryCredential = 'skanderbac' 
+        dockerImage = '' 
+    }
+    
+	agent any
+
+	stages{
+		       
+			stage('Clean Install'){
+				steps{
+					bat "mvn clean install -DskipTests"
+				}				
+			}
+
+
+			stage('Sonar Analyse'){
+				steps{
+                    bat "mvn sonar:sonar -DskipTests"
+                  }
+            }
+            
+            stage('Nexus Deploy'){
+				steps{
+                    bat "mvn deploy -DskipTests"
+                  }
+            }
+            
+            stage('Building our image') { 
+                steps { 
+                    script { 
+                    dockerImage = docker.build("$registry:$BUILD_NUMBER")
+                    }
+                } 
+            }
+
+           stage('Deploy our image') { 
+                steps { 
+                    script { 
+                    docker.withRegistry( '', registryCredential ) { 
+                        dockerImage.push() 
+                    }
+                } 
+             }
+           } 
+          
+           stage('Cleaning up') { 
+                steps { 
+                    bat "docker rmi $registry:$BUILD_NUMBER" 
+                }
+           } 
+
+		    stage('Pulling from docker hub') { 
+                steps { 
+                    script { 
+                    docker.withRegistry( '', registryCredential ) { 
+                        dockerImage.pull() 
+                    }
+                } 
+             }
+           } 
+
+        
+        	stage('mysql') { 
+                steps { 
+                    bat "docker container run --name mysqldb --network devops-nt  -e MYSQL_ROOT_PASSWORD=root -e MYSQL_DATABASE=timesheet -d mysql:5.6"
+
+                }
+           } 
+           stage('run images') { 
+                steps { 
+                    bat "docker container run --network devops-nt --name timesheet-container -p 8083:8083 -d $registry:$BUILD_NUMBER"
+                }
+           } 
+	}
+	
+	  post{
+            always{        
+                cleanWs()
+        }
+    }
+	 
+
+}
--- src/main/java/tn/esprit/spring/services/EntrepriseServiceImpl.java
+++ src/main/java/tn/esprit/spring/services/EntrepriseServiceImpl.java
@@ -3,6 +3,7 @@ package tn.esprit.spring.services;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -20,14 +21,25 @@ public class EntrepriseServiceImpl implements IEntrepriseService {
 	@Autowired
 	DepartementRepository deptRepoistory;
 	
+	private static final Logger logger = Logger.getLogger(EntrepriseServiceImpl.class);
+	
 	public int ajouterEntreprise(Entreprise entreprise) {
 		entrepriseRepoistory.save(entreprise);
 		return entreprise.getId();
 	}
 
 	public int ajouterDepartement(Departement dep) {
-		deptRepoistory.save(dep);
-		return dep.getId();
+		try {
+			deptRepoistory.save(dep);
+			logger.info("departement ajouté");
+			return dep.getId();
+		}
+		catch(Exception e) {
+			
+			logger.error("probleme lors de l'insertion de departement:"+e);
+			return 0;
+		}
+		
 	}
 	
 	public void affecterDepartementAEntreprise(int depId, int entrepriseId) {
@@ -36,22 +48,37 @@ public class EntrepriseServiceImpl implements IEntrepriseService {
 				// ==> c'est l'objet departement(le master) qui va mettre a jour l'association
 				//Rappel : la classe qui contient mappedBy represente le bout Slave
 				//Rappel : Dans une relation oneToMany le mappedBy doit etre du cote one.
-				Entreprise entrepriseManagedEntity = entrepriseRepoistory.findById(entrepriseId).get();
-				Departement depManagedEntity = deptRepoistory.findById(depId).get();
-				
-				depManagedEntity.setEntreprise(entrepriseManagedEntity);
-				deptRepoistory.save(depManagedEntity);
+			
+		try {
+			Entreprise entrepriseManagedEntity = entrepriseRepoistory.findById(entrepriseId).get();
+			Departement depManagedEntity = deptRepoistory.findById(depId).get();
+			
+			depManagedEntity.setEntreprise(entrepriseManagedEntity);
+			deptRepoistory.save(depManagedEntity);
+			logger.info("departement num "+depId+"affecte a  l'entreprise num "+entrepriseId);
+		}
+		catch(Exception e) {
+			logger.error("probleme d'affectation:"+e);
+		}
 		
 	}
 	
 	public List<String> getAllDepartementsNamesByEntreprise(int entrepriseId) {
-		Entreprise entrepriseManagedEntity = entrepriseRepoistory.findById(entrepriseId).get();
 		List<String> depNames = new ArrayList<>();
-		for(Departement dep : entrepriseManagedEntity.getDepartements()){
-			depNames.add(dep.getName());
+		try {
+			Entreprise entrepriseManagedEntity = entrepriseRepoistory.findById(entrepriseId).get();
+			for(Departement dep : entrepriseManagedEntity.getDepartements()){
+				depNames.add(dep.getName());
+			}
+			logger.info("liste:"+depNames);
+			
 		}
-		
+		catch(Exception e) {
+			
+			logger.error("probleme:"+e);
+		}
 		return depNames;
+		
 	}
 
 	@Transactional
